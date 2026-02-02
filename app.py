bd191521bcff6e208470e226c0f7ed98f53ba04d
aline-hoffmann <alinehoffmannp@gmail.com>
2026-02-02 18:29:04 -0300

    alteração da localização do csv

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ st.set_page_config(
 )
 
 # --- Carregamento dos dados ---
-df = pd.read_csv("../salaries_limpo.csv")
+df = pd.read_csv("./salaries_limpo.csv")
 
 # --- Barra Lateral (Filtros) ---
 st.sidebar.header("🔍 Filtros")
